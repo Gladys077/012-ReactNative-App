5f6c5fb666f30530a3fcf57aa51a92f46de50f6e
Gladys Romero <111000044+Gladys077@users.noreply.github.com>
2025-09-22 21:30:23 -0300

    corregí pressed al componente googleButton

--- components/UI/Button/ButtonGoogle.tsx
+++ components/UI/Button/ButtonGoogle.tsx
@@ -1,4 +1,3 @@
-
 import { BorderRadius } from '@/constants/Tokens';
 import { useTheme } from '@/context/ThemeContext';
 import { Pressable, Text, View } from 'react-native';
@@ -9,24 +8,35 @@ interface ButtonGoogleProps {
   children?: string;
 }
 
-const ButtonGoogle = ({  onLogin, children = "Sign in with Google" }: ButtonGoogleProps) => {
-    const { colors } = useTheme(); 
+const ButtonGoogle = ({ onLogin, children = "Sign in with Google" }: ButtonGoogleProps) => {
+  const { colors } = useTheme();
 
   return (
     <Pressable
       onPress={onLogin}
-      className="w-full flex-row items-center justify-center px-4 h-14 border shadow-md"
-      style={{ 
-        borderRadius: BorderRadius.pillBtn, 
-        borderWidth: 1, 
-        borderColor: colors.border, 
-      }}
+      className="w-full"
     >
-      <View className="flex-row items-center justify-center">
-        <GoogleLogoColor height={20} className="mx-2"/>
-        <Text className="font-medium"
-        style={{color: colors.textDefault}}>{children}</Text>
-      </View>
+      {({ pressed }) => (
+        <View
+          className="flex-row items-center justify-center px-4 h-14 border shadow-md"
+          style={{
+            borderRadius: BorderRadius.pillBtn,
+            borderWidth: 1,
+            borderColor: colors.border,
+            transform: [{ scale: pressed ? 0.95 : 1 }],
+            opacity: pressed ? 0.9 : 1,
+            backgroundColor: colors.cardBg,
+          }}
+        >
+          <GoogleLogoColor height={20} className="mx-2" />
+          <Text
+            className="font-medium"
+            style={{ color: colors.textDefault }}
+          >
+            {children}
+          </Text>
+        </View>
+      )}
     </Pressable>
   );
 };
