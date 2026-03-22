8b2f6830f5a0ddc0ddf426c0b5844497a7e1037a
lionel staricoff <staricofflionel@hotmail.com>
2026-03-22 16:51:22 -0500

    fix: arreglando incompatibilidades

--- app/(preAuth)/_layout.tsx
+++ app/(preAuth)/_layout.tsx
@@ -1,46 +1,53 @@
+import "react-native-reanimated"; // 👈 MUY IMPORTANTE (PRIMERA LÍNEA)
+
 import { Spacing } from "@/constants/Tokens";
 import { useTheme } from "@/context/ThemeContext";
 import { Slot, useSegments } from "expo-router";
 import { View } from "react-native";
 import Header from "../../components/UI/Header";
 
+import { GestureHandlerRootView } from "react-native-gesture-handler"; // 👈 NUEVO
+
 export default function PreAuthLayout() {
   const { colors } = useTheme();
-  const segments = useSegments(); // Obtiene la ruta actual como array
-  
-  const currentPage = segments[segments.length - 1] as string; // extrae el último segmento (nombre de la screen)
-  
+  const segments = useSegments();
+
+  const currentPage = segments[segments.length - 1] as string;
+
   const getTitleByPage = () => {
     switch (currentPage) {
-      case 'registro': 
-        return 'Registro';
-      case 'perfil': 
-        return 'Perfil';
-      case 'cambiarContrasena': 
-        return 'Cambiar Contraseña';
-      case 'olvideContrasena': 
-        return 'Recuperar Contraseña';
-      default: 
-        return '';
+      case "registro":
+        return "Registro";
+      case "perfil":
+        return "Perfil";
+      case "cambiarContrasena":
+        return "Cambiar Contraseña";
+      case "olvideContrasena":
+        return "Recuperar Contraseña";
+      default:
+        return "";
     }
   };
-  
+
   return (
-    
-    <View style={{ flex: 1, backgroundColor: colors.background }}>
-      <Header showBackArrow title={getTitleByPage()} />
-      
-      <View
-        style={{
-          flex: 1,
-          paddingHorizontal: Spacing.xl,   
-          maxWidth: 500,                  
-          width: "100%",
-          alignSelf: "center",              
-        }}
-      >
-        <Slot />
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      {" "}
+      {/* 👈 CLAVE */}
+      <View style={{ flex: 1, backgroundColor: colors.background }}>
+        <Header showBackArrow title={getTitleByPage()} />
+
+        <View
+          style={{
+            flex: 1,
+            paddingHorizontal: Spacing.xl,
+            maxWidth: 500,
+            width: "100%",
+            alignSelf: "center",
+          }}
+        >
+          <Slot />
+        </View>
       </View>
-    </View>
+    </GestureHandlerRootView>
   );
 }
